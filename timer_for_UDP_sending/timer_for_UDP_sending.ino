41ceb5ed8319e31196260ccc3c400f80714b6560
richardballaux <45049937+richardballaux@users.noreply.github.com>
2019-03-31 13:10:06 +0200

    Update timer_for_UDP_sending.ino
    added small feedback for testing

--- timer_for_UDP_sending/timer_for_UDP_sending.ino
+++ timer_for_UDP_sending/timer_for_UDP_sending.ino
@@ -1,7 +1,10 @@
 unsigned long oldLoopTime = 0;
 unsigned long newLoopTime = 0;
 unsigned long neededElapsedTime = 10;
+unsigned long lastTimeSent = 0;
+unsigned long nowSent = 0;
 void setup() {
+  Serial.begin(115200);
   // put your setup code here, to run once:
 
 }
@@ -9,7 +12,11 @@ void setup() {
 void loop() {
   
   if(timeToSendUdp){
-    Serial.println("I now send UDP");
+    Serial.print("Time since last sending: ");
+    nowSent = millis();
+    Serial.print(nowSent - lastTimeSent);
+    lastTimeSent = nowSent;
+    Serial.println("\t I now send UDP");
   }
 
 }
